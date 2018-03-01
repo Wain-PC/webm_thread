6e74db5e7387bc79ed756ff63332a6dc760f7b66
Wain-PC <wain-pc@mail.ru>
2018-03-01 16:23:40 +0300

    -Added `Parser` module

--- finder/index.js
+++ finder/index.js
@@ -2,7 +2,7 @@ const request = require('request-promise-native');
 const rabbit = require('./utils/rabbit');
 const config = require('config').get('webmthread');
 
-const catalogUrl = config.urls.catalog;
+const catalogUrl = config.finder.catalog;
 const webmRegExp = /WEBM|ВЕБМ|ШЕБМ|ШЕВМ|MP4|МР4/i;
 const onMessage = (message) => {
     console.log("Received message from RabbitMQ:", message);
@@ -14,21 +14,30 @@ const loadThreads = () => {
         method: 'GET',
         uri: catalogUrl,
         json: true
-    }).then(({threads}) => {
-        threads.map(({num, subject, comment, posts_count, files_count})=>{
-            const subjComment = `${subject} ${comment}`;
-            if(webmRegExp.test(subjComment)) {
-                console.log(`Found WEBM thread with
+    }).then(({threads}) => threads.reduce(function (webmThreads, thread) {
+        const {num, subject, comment, posts_count, files_count} = thread;
+        const subjComment = `${subject} ${comment}`;
+        if (webmRegExp.test(subjComment)) {
+            console.log(`Found WEBM thread with
                 ID: ${num}
                 Subject: ${subject}
                 Comment: ${comment}
                 Posts: ${posts_count}
                 Files: ${files_count}
                 `);
-                rabbit.publish({id: num, subject, comment, posts: posts_count, files: files_count});
-            }
+            webmThreads.push({boardId: 'b', threadId: num});
+        }
+        return webmThreads;
+    }, []))
+    // Do not send all videos at once to prevent backend overload. Send one thread every N seconds
+    // (value taken from config).
+        .then(webmThreads => {
+            webmThreads.map((thread, i) => setTimeout(() => {
+                console.log(`Pushing thread ${thread.threadId} to Parser`);
+                rabbit.publish(thread);
+            }, config.finder.threadParseInterval * i));
+
         });
-    }, err => console.error(err));
 };
 
 
---
+++ parser/utils/rabbit.js
@@ -0,0 +1,84 @@
+const amqp = require('amqplib');
+const url = require('url');
+const uuid = require('uuid/v1');
+
+const config = require('config').get('webmthread').get('rabbitMQ');
+
+let channelPromise;
+const channelRoutingKey = uuid();
+
+const subscribeToExchange = (channel, callback) => {
+    const {subscribe} = config.exchanges;
+    channel.assertExchange(subscribe, 'fanout');
+    channel.assertQueue(null, {durable: false, autoDelete: true})
+        .then(({queue}) => {
+            channel.bindQueue(queue, subscribe, channelRoutingKey);
+            channel.consume(queue, callback, {noAck: true});
+            return channel;
+        });
+};
+
+const connectionInitialize = (url) => amqp.connect(url)
+    .catch((err) => new Promise(resolve => {
+        console.log(`Rabbit is unavailable (${err}), waiting to try again in ${config.reconnectTimeout/1000} seconds `);
+        setTimeout(() => resolve(connectionInitialize(url)), config.reconnectTimeout);
+    }));
+
+const connect = (onMessage = () => {
+}) => {
+    const {
+        username, password, hostname, port,
+    } = config;
+    const connectionURL = url.format({
+        protocol: 'amqp',
+        slashes: true,
+        hostname,
+        port,
+        auth: `${username}:${password}`,
+    });
+    channelPromise = connectionInitialize(connectionURL)
+        .then(connection => {
+            console.log('Creating Channel');
+            return connection.createChannel();
+        })
+        .then(channel => {
+            const {publish, subscribe} = config.exchanges;
+            console.log("Got channel");
+            if (publish) {
+                console.log(`Found publish ability to exchange ${publish}, creating exchange...`);
+                channel.assertExchange(publish, 'fanout');
+            }
+            if (subscribe) {
+                console.log(`Found subscription to exchange ${subscribe}, subscribing...`);
+                return subscribeToExchange(channel, (message) => {
+                    const {content} = message;
+                    onMessage(JSON.parse(content.toString("utf-8")));
+                });
+            }
+            return channel;
+        });
+    return channelPromise;
+};
+
+const publish = (payloadObj) => {
+    const {publish} = config.exchanges;
+    if (!channelPromise) {
+        return Promise.reject(new Error('No RabbitMQ connection available to publish'));
+    }
+    if (!publish) {
+        return Promise.reject(new Error('No `publish` exchange set in config, nowhere to publish the message.'));
+    }
+
+    const correlationId = uuid();
+
+    return channelPromise
+        .then(channel => channel.publish(publish, config.routingKey, new Buffer(JSON.stringify(payloadObj)), {
+            deliveryMode: 2,
+            contentType: 'application/json',
+            replyTo: channelRoutingKey,
+            correlationId,
+        })).catch(err => console.error(err));
+};
+
+module.exports.publish = publish;
+module.exports.connect = connect;
