62d8a0921d14a0346333a720c346928c91443f37
Wain-PC <wain-pc@mail.ru>
2018-03-02 19:17:00 +0300

    -Moved to MongoDB

--- db/index.js
+++ db/index.js
@@ -1,22 +1,12 @@
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
+const MongoClient = require('mongodb').MongoClient;
 const rabbit = require('./utils/rabbit');
 const config = require('config').get('webmthread');
-const models = {};
-let sequelize;
+let db;
 
 const api = {
     getSources: () => Promise.resolve([{url: 'https://2ch.hk/b/catalog_num.json', description: '2ch WEBM Threads'}]),
-    saveThread: thread => models.thread.findOrCreate({where: {url: thread.url}, defaults: thread}),
-    saveVideos: ({threadUrl, videos}) => models.thread.findOne({
-        where: {
-            url: threadUrl
-        }
-        //TODO: Refactor batch save
-    }).then(thread => thread ? Promise.all(videos.map(video => models.video.findOrCreate({
-        where: {url: video.url},
-        defaults: video
-    }))) : [])
+    saveThread: () => Promise.resolve({}),
+    saveVideos: () => Promise.resolve({}),
 };
 
 const onMessage = ({type, payload}, correlationId, replyTo) => {
@@ -33,76 +23,25 @@ const onMessage = ({type, payload}, correlationId, replyTo) => {
     }, 100);
 };
 
+const createCollections = () => db.createCollection('sources');
+
 /**
  * Connects to the DB (sqlite@localhost)
- * @returns sequelize instance
+ * @returns Promise<Db> instance
  */
-const connectToDb = () =>
-    new Promise((resolve, reject) => {
-        const {name, user, password, host, dialect, pool, storage, logging} = config.db;
-        try {
-            resolve(new Sequelize(name, user, password, {host, dialect, pool, storage, logging}));
-        } catch (err) {
-            reject(err);
-        }
-    });
-
-/**
- * Creates data models in DB
- * @param [force] If true, this will purge the data in the DB.
- */
-createModels = (force = false) => {
-    models.source = sequelize.define('source', {
-        url: {
-            type: Sequelize.STRING,
-            primaryKey: true,
-            allowNull: false
-        },
-        description: {
-            type: Sequelize.STRING,
-        }
-    });
-
-
-    models.thread = sequelize.define('thread', {
-        url: {
-            type: Sequelize.STRING,
-            primaryKey: true,
-            allowNull: false
-        }
-    });
-
-    models.video = sequelize.define('video', {
-        url: {
-            type: Sequelize.STRING,
-            primaryKey: true,
-            allowNull: false
-        },
-        thumbnailUrl: {
-            type: Sequelize.STRING
-        },
-        displayName: {
-            type: Sequelize.STRING
-        }
-    });
-
-    models.source.hasMany(models.thread, {onDelete: 'cascade'});
-    models.thread.hasMany(models.video, {onDelete: 'cascade'});
-
-    return Promise
-        .all(Object.keys(models)
-            .map(key => models[key].sync({force})))
-        .then(() => models);
+const connectToDb = () => {
+    const {host, port, dbName} = config.db;
+    return MongoClient.connect(`mongodb://${host}:${port}`)
+        .then(dataBase => dataBase.db(dbName))
+        .then(instance => {
+            db =instance;
+            return createCollections();
+        });
 };
 
 console.log("Starting Rabbit connection!");
 connectToDb()
-    .then(sequelizeInstance => {
-        console.log("DB Connection instantiated");
-        sequelize = sequelizeInstance;
-        createModels();
-        return rabbit.connect(onMessage);
-    })
+    .then(()=>rabbit.connect(onMessage))
     .then(() => {
         console.log("RabbitMQ and DB Connection established");
     }, (err) => {
